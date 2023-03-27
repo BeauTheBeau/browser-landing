7f362abdf568e515ac7cca02295dfda13973f138
beau <beau7476@gmail.com>
2023-03-27 23:42:34 +0100

    Full redesign - looks much cooler

--- js/main.js
+++ js/main.js
@@ -1,154 +1,36 @@
-// rember https://www.google.com/s2/favicons?domain=
+const
+    TIME = document.getElementById('time'),
+    SEARCH = document.getElementById('search'),
+    SEARCH_BUTTON = document.getElementById('search-button'),
+    BG_IMG = document.getElementById("background__image");
 
-/*
-	==========================================
-	LOCAL STORAGE
-	==========================================
- */
+const 
+    NEW_IMAGE = document.getElementById('new__image');
 
-// Get SETTINGS from local storage
-let SETTINGS = JSON.parse(localStorage.getItem('SETTINGS'));
-let QUERIES = JSON.parse(localStorage.getItem('QUERIES'));
+// Every second, update the time
+setInterval(() => {
+    let date = new Date();
+    TIME.innerHTML = `${date.getHours()}:${date.getMinutes()}`;
 
-// If SETTINGS is null, set it to default
-if (SETTINGS === null) {
-	SETTINGS = {
-		title: 'Default Title',
-		recentlyViewed: {
-			enabled: true,
-			max: 5
-		},
-		search: {
-			enabled: true,
-			engines: [
-				{
-					name: 'Google',
-					url: 'https://www.google.com/search?q='
-				},
-				{
-					name: 'DuckDuckGo',
-					url: 'https://duckduckgo.com/?q='
-				},
-				{
-					name: 'Bing',
-					url: 'https://www.bing.com/search?q='
-				},
-				{
-					name: 'Yahoo',
-					url: 'https://search.yahoo.com/search?p='
-				},
-				{
-					name: 'Yandex',
-					url: 'https://yandex.com/search/?text='
-				},
-				{
-					name: 'YouTube',
-					url: 'https://www.youtube.com/results?search_query='
-				},
-				{
-					name: 'Wikipedia',
-					url: 'https://en.wikipedia.org/w/index.php?search='
-				}
-			],
-			default: 1
-		}
+    if (date.getMinutes() < 10) TIME.innerHTML = `${date.getHours()}:0${date.getMinutes()}`;
+    if (date.getHours() < 10) TIME.innerHTML = `0${date.getHours()}:${date.getMinutes()}`;
+}, 20);
 
-	}
-}
-localStorage.setItem('SETTINGS', JSON.stringify(SETTINGS));
-
-if (QUERIES === null) {
-	QUERIES = {};
-}
-localStorage.setItem('QUERIES', JSON.stringify(QUERIES));
-
-/*
-	==========================================
-	VARIABLES
-	==========================================
- */
-
-const elements = {
-	title: document.getElementById('title'),
-	search: document.getElementById('search_tab'),
-	recentlyViewed: document.getElementById('recently_viewed')
-}
-
-/*
-	==========================================
-	MISC
-	==========================================
- */
-
-document.title = SETTINGS.title;
-elements.search.placeholder = "Search " + SETTINGS.search.engines[SETTINGS.search.default].name;
-
-/*
-	==========================================
-	SEARCH
-	==========================================
- */
-
-function search(query) {
-	if (query === '') return console.log('No query');
-
-	// Add query to QUERIES
-	if (QUERIES[query] === undefined) QUERIES[query] = 1;
-	else QUERIES[query]++;
-
-	localStorage.setItem('QUERIES', JSON.stringify(QUERIES));
-
-	// If it begins with a protocol, redirect to it
-	if (query.startsWith('http://') || query.startsWith('https://')) window.location.href = query;
-	else window.location.href = SETTINGS.search.engines[SETTINGS.search.default].url + query;
-}
-
-elements.search.addEventListener('keydown', (e) => {	
-	if (e.key === 'Enter') search(elements.search.value);
+// on any keyboard input, focus the search bar - except tab / shift tab
+document.addEventListener('keydown', (e) => {
+    if (e.key !== 'Tab' && e.key !== 'Shift' && e.key !== 'Control' && e.key !== 'Alt') SEARCH.focus();
+    if (e.key === 'Enter') SEARCH_BUTTON.click();
 });
 
-/*
-	==========================================
-	RECENTLY VIEWED
-	==========================================
- */
+// on Search button click, open a new tab with the search query in current tab
+SEARCH_BUTTON.addEventListener('click', () => {
+    window.open(`https://google.com/search?q=${SEARCH.value}`, '_self');
+});
 
-function updateRecentlyViewed() {
-	// Clear the list
-	elements.recentlyViewed.innerHTML = '';
-
-	// Sort QUERIES by value
-	let sortedQueries = Object.keys(QUERIES).sort((a, b) => QUERIES[b] - QUERIES[a]);
-
-	// Loop through sorted QUERIES
-	for (let i = 0; i < sortedQueries.length; i++) {
-		// If the max has been reached, stop
-		if (i >= SETTINGS.recentlyViewed.max) break;
-
-		// Create list item
-		let li = document.createElement('li');
-		li.innerHTML = sortedQueries[i];
-		li.addEventListener('click', () => search(sortedQueries[i]));
-
-		const favicon = document.createElement('img');
-		// quality of 512
-		favicon.src = `https://www.google.com/s2/favicons?domain=${sortedQueries[i]}&sz=16`;
-		favicon.alt = sortedQueries[i] + ' favicon'; 
-		favicon.width = 16;
-		favicon.height = 16;
-		li.prepend(favicon);
-		
-		
-		// Append list item to list
-		elements.recentlyViewed.appendChild(li);
-	}
-}
-
-updateRecentlyViewed();
-
-/*
-	==========================================
-	CONTEXT MENU
-	==========================================
- */
-
+// on new image button click, change the background image
+NEW_IMAGE.addEventListener('click', () => {
+    console.log("SEARCH.value")
+    if (SEARCH.value === '') SEARCH.value = 'nature';
+    BG_IMG.src = `https://source.unsplash.com/1920x1080/?${SEARCH.value}`;
+    
+});
